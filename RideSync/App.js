628af4106e3dd6ff987a3daf780adfc3b20130a5
VinBid <115661286+VinBid@users.noreply.github.com>
2023-05-21 10:17:59 -0700

    MongoDB react implementation

--- RideSync/App.js
+++ RideSync/App.js
@@ -1,9 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-const connectAndQuery = require('./mongodb');
+// import {login, connectAndQuery} from './Mongo';
+// import {HomeComponent} from "./Homepage"
+
+// login('bob', 'abc', 'r');
+// connectAndQuery('bob', 'abc');
+
+
+
+
+
 //connectAndQuery(); HOW TO DO OPERATIONS WITH MONGODB
 export default function App() {
+  // connectAndQuery('Bob', 'abc', 'd');
+  // login('Bob', 'abc');
   return (
+
     <View style={styles.container}>
       <Text>Hello this is my application</Text>
       <StatusBar style="auto" />
--- RideSync/Mongo.js
+++ RideSync/Mongo.js
@@ -1,14 +1,40 @@
+
+
+
 const { MongoClient } = require('mongodb');
 
-const uri = '646925f364e77666a558da45';
-const client = new MongoClient(uri);
+// const uri = '646925f364e77666a558da45';
+const uri = "mongodb+srv://VinBid:<RideSync123>@cluster0.xxo8hxt.mongodb.net/?retryWrites=true&w=majority"
+// const client = new MongoClient(uri);
 
-async function connectAndQuery() {
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+export async function connectAndQuery(Username, password, status) {
   try {
     await client.connect();
 
     const database = client.db('your-database-name');
-    const collection = database.collection('your-collection-name');
+    const collection = database.collection();
+    //google places API Google Autocomplete API
+    //google maps API
+    //google maps api counts as google cloud!
+    //create a person object
+    db.collection.insertOne(
+      {
+        id: Username,
+        pass: password,
+        driverorpass: status,
+        longitude: null,
+        latitude: null
+
+      }
+    )
 
     // Perform database operations here
     const documents = await collection.find().toArray();
@@ -18,4 +44,36 @@ async function connectAndQuery() {
   }
 }
 
+
+export async function login(Username, password) {
+  try {
+    console.log("connecting");
+    await client.connect();
+    console.log("connected");
+    const database = client.db('your-database-name');
+    const collection = database.collection();
+    const query = {Username};
+    const result = await collection.find(query).toArray();
+    console.log(result);
+    if(result != null) {
+      query = {password};
+      result = result.find(query).toArray;
+      if(result == null) {
+        console.log("error");
+      }
+      else {
+        console.log("success");
+      }
+    }
+
+  } finally {
+    await client.close();
+  }
+
+
+
+
+}
+
 connectAndQuery();
+login();
